a972706933e30c7156d784553eb97d41be9143fd
sundev-nalees <72244488+sundev-nalees@users.noreply.github.com>
2023-02-16 09:59:11 +0530

    phase1

--- Assets/Script/Player/PlayerControl.cs
+++ Assets/Script/Player/PlayerControl.cs
@@ -12,6 +12,8 @@ public class PlayerControl : MonoBehaviour
     private float horizontal;
     private float vertical;
     private PhotonView view;
+    private int playerId;
+    private int score = 1;
 
     private void Start()
     {
@@ -33,5 +35,24 @@ public class PlayerControl : MonoBehaviour
         vertical = Input.GetAxisRaw("Vertical");
         Vector3 directions = new Vector3(horizontal, 0.0f, vertical) * playerSpeed * Time.deltaTime;
         transform.Translate(directions);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<coinCollection>() == true)
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                UIManager.instance.UpdateScore(1,score);
+            }
+            else
+            {
+                UIManager.instance.UpdateScore(2, score);
+            }
+           /* ExitGames.Client.Photon.Hashtable playerProperties = PhotonNetwork.LocalPlayer.CustomProperties;
+            playerProperties["score"] = (int)playerProperties["score"] + 1;
+            PhotonNetwork.SetPlayerCustomProperties(playerProperties);*/
+        }
+        
     }
 }
--- Assets/Script/spawners/CoinSpawner.cs
+++ Assets/Script/spawners/CoinSpawner.cs
@@ -6,9 +6,11 @@ using Photon.Pun;
 public class CoinSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject coinPrefab;
-    [SerializeField] private float numberOfCoins;
+    [SerializeField] private int numberOfCoins;
     [SerializeField] private float fieldLength=430f;
     [SerializeField] private float fieldBreadth=220f;
+
+    private GameObject[] coins;
 
     void Start()
     {
@@ -20,12 +22,13 @@ public class CoinSpawner : MonoBehaviour
     {
         if (PhotonNetwork.IsMasterClient)
         {
+            coins = new GameObject[numberOfCoins];
             for (int i = 0; i < numberOfCoins; i++)
             {
                 float x = Random.Range(-fieldLength / 2, fieldLength / 2);
                 float z = Random.Range(-fieldBreadth / 2, fieldBreadth / 2);
                 Vector3 spawnPosition = new Vector3(x, 13, z);
-                PhotonNetwork.Instantiate(coinPrefab.name, spawnPosition, Quaternion.identity);
+                coins[i]=PhotonNetwork.Instantiate(coinPrefab.name, spawnPosition, Quaternion.identity);
             }
         }
         
